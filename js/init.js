948d225acdb1d818882cd0b1b05167622f5c1eae
Matthieu <matthieuraymond@hotmail.com>
2016-07-28 14:10:51 +0200

    Add download button

--- js/init.js
+++ js/init.js
@@ -5,4 +5,10 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+
+  // Link modal
+  $(document).ready(function(){
+    $('.modal-trigger').leanModal();
+  });
+
 })(jQuery); // end of jQuery name space
